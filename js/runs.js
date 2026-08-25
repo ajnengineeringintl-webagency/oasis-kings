3873b7733acd095e6a2a57198374b7f67a5d1bf2
ajnengineeringintl-webagency <ajnengineeringintl@gmail.com>
2026-08-25 13:04:18 -0700

    first co

---
+++ js/runs.js
@@ -0,0 +1,23 @@
+//JESUS IS LORD
+(function(){
+    emailjs.init({
+        PublicKeyCredential:"DffGOUANLHoHxj1iI"
+    })
+})
+window.onload = function(){
+    const frm = document.getElementById("frm")
+
+    frm.addEventListener("submit",(event)=>{
+        event.preventDefalt();
+
+        emailjs.sendForm("service_ttte4tm","template_g7uypn1",this)
+            .then(()=>{
+                this.alert("Thanks! Message Submitted.")
+                frm.reset();
+            }),
+                (error)=>{
+                    this.alert("Error! Message Submitted.")
+                    console.error(error)
+                }
+    })
+}
