5e730515817590fd053fe4726f0a446db7d22942
austin <ajnengineeringintl@gmail.com>
2026-09-04 12:11:50 -0700

    .

--- js/runs.js
+++ js/runs.js
@@ -1,24 +1,37 @@
 //JESUS IS LORD
 //import emailjs from "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js";
-(function(){
-    emailjs.init({
-        PublicKeyCredential:"DffGOUANLHoHxj1iI"
-    })
-})
+
+const frm = document.getElementById("frm")
 window.onload = function(){
-    const frm = document.getElementById("frm")
 
-    frm.addEventListener("submit",(event)=>{
-        event.preventDefalt();
-
-        emailjs.sendForm("service_ttte4tm","template_g7uypn1",this)
-            .then(()=>{
-                this.alert("Thanks! Message Submitted.")
-                frm.reset();
-            }),
-                (error)=>{
-                    this.alert("Error! Message Submitted.")
-                    console.error(error)
+    frm.addEventListener("submit",async function(event){
+        event.preventDefault();
+        const name= document.getElementById("name").value;
+        const email= document.getElementById("email").value;
+        const title= document.getElementById("title").value;
+        const number= document.getElementById("number").value;
+        const message= document.getElementById("message").value;
+        const py = {
+            name: name,
+            email: email,
+            title: title,
+            se: number,
+            message: message
+        };
+        try {
+            const res = await fetch("https://oasis-backend-server-ndlo.vercel.app/api/send-mail",
+                {
+                    method: "POST",
+                    headers: {
+                        'content-type':'application/json'
+                    },body: JSON.Stringify(py)
                 }
+            );
+            const r = await res.json();
+            console.log(r);
+         } catch (error) {
+            console.log(error);
+            
+        };
     })
 }
